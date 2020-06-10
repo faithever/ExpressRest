34dcb2121ad2e9d6a0c2556fa6bce640b71ab3ce
Owen Lee <owen_lee@kaonmedia.com>
2020-06-10 19:29:47 +0900

    just add space

--- logger.js
+++ logger.js
@@ -1,7 +1,7 @@
 
 const logger = function(req, res, next){
    //console.log(`${req.protocol}::\/\/${req.get('host')}`);
-   console.log("connection:" + `${req.protocol}::\/\/${req.get('host')}${req.originalUrl}`);
+   console.log("connection: " + `${req.protocol}::\/\/${req.get('host')}${req.originalUrl}`);
    next();
 };
 
