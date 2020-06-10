7eb6523043c3e95407aa0c97db3e6ba0e5d303de
Owen Lee <owen_lee@kaonmedia.com>
2020-06-10 19:12:00 +0900

    add Members.js

---
+++ Members.js
@@ -0,0 +1,23 @@
+
+const members = [
+{
+   'id': 1,
+   'name':'owen1',
+   'email':'owen1.members.com'
+},
+{
+   'id': 2,
+   'name':'owen2',
+   'email':'owen2.members.com'
+},
+{
+   'id': 3,
+   'name':'owen3',
+   'email':'owen3.members.com'
+},
+
+];
+
+
+module.exports = members;
+
